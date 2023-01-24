e36aa8efd8b27005fe92511b096398447114bda0
Svetlana Shmeleva <svshmelyova@gmail.com>
2023-01-24 12:01:38 +0000

    Fixed Create table and Add new book
    Fixed a bug that appeared when CREATE and INSERT INTO the table "books"
    when table "books" was already exists.
    In function enter() "Add new book" added and implemented a check for the
    repetition of a "id" value

--- bookstore.py
+++ bookstore.py
@@ -4,13 +4,21 @@ db = sqlite3.connect('ebookstore')
 cursor = db.cursor()
 
 def enter():
-    new_id = input("Enter id:  ")
     new_title = input("Title:  ")
     new_author = input("Author:  ")
     new_qty = input("Quantity:  ")
-    new_book = [new_id, new_title, new_author, new_qty]
-    db.execute('''INSERT INTO books VALUES(?,?,?,?)''', new_book)
-    db.commit()
+
+    while True:
+        new_id = input("Enter id:  ")
+        try:
+            new_book = [new_id, new_title, new_author, new_qty]
+            db.execute('''INSERT INTO books VALUES(?,?,?,?)''', new_book)
+            db.commit()
+            break
+        except sqlite3.IntegrityError:
+            print("this id already is in database. Enter new id:")
+            continue
+
 
 """def update():
     menu = input(What do you want to update: 
@@ -30,8 +38,7 @@ def delete():
     for row in cursor:
         sure = input(f"""Do you want to delete this book: 
     {'{0} : {1} : {2}'.format(row[0], row[1], row[2])}
-y or n:
-              """)
+y or n: """)
         if sure == "y":
             print(f"""Book: 
     {'{0} : {1} : {2}'.format(row[0], row[1], row[2])}
@@ -78,15 +85,19 @@ def view_all():
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY, Title TEXT NOT NULL, Author TEXT NUT NULL, Qty INT DEFAULT 0)''')
+cursor.execute("""SELECT * FROM books""")
+row = cursor.fetchone()
+if row is None:
+    books = [(3001, "A Tale of Two Cities", "Charles Dickens", 30), 
+            (3002, "Harry Potter and the Philosopher's Stone", "J.K. Rowling", 40), 
+            (3003, "The Lion, the Witch and the Wardrobe", "C.S. Lewis", 25), 
+            (3004, "The Lord of the Rings", "J.R.R Tolkien", 37), 
+            (3005, "Alice in Wonderland", "Lewis Carroll", 12)]
+
+    cursor.executemany(''' INSERT INTO books(id, Title, Author, Qty) VALUES(?,?,?,?)''', books)
 db.commit()
 
-books = [(3001, "A Tale of Two Cities", "Charles Dickens", 30), 
-        (3002, "Harry Potter and the Philosopher's Stone", "J.K. Rowling", 40), 
-        (3003, "The Lion, the Witch and the Wardrobe", "C.S. Lewis", 25), 
-        (3004, "The Lord of the Rings", "J.R.R Tolkien", 37), 
-        (3005, "Alice in Wonderland", "Lewis Carroll", 12)]
 
-cursor.executemany(''' INSERT INTO books(id, Title, Author, Qty) VALUES(?,?,?,?)''', books)
 
 while True:
     menu = input("""\nWhat do you intend to do:
@@ -120,3 +131,5 @@ while True:
         print("Wrong enter. Try again please.")
 
 print("Thank you for using BOOK Database")
+
+db.close()
